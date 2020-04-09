5418aa167503cf9de1d8f5fae8169f03a0f49149
github username <github email address>
2020-04-09 14:11:54 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,14 @@
 class BirdsController < ApplicationController
+  def index
+    birds = Bird.all
+    render json: BirdSerializer.new(birds)
+  end
+
+  def show  
+    bird = Bird.find_by(id: params[:id])
+    options = {
+      include: [:sightings, :locations]
+    }
+    render json: BirdSerializer.new(bird, options)
+  end
 end
